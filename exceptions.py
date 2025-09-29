708e9d221b967517c121b8d7a484983f47065c2d
zchen048 <zjchen048@gmai.com>
2025-09-29 11:18:19 +0800

    Added CollectionNotFound and ChunkIDInvalid to exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -7,6 +7,22 @@ class NoFieldsToUpdateException(Exception):
     pass
 
 # ====================
+# chroma utils exceptions
+# ====================
+
+class CollectionNotFoundException(Exception):
+    """Raised when a requested collection name is not found in Chroma db"""
+    def __init__(self, collection_name: str):
+        self.collection_name = collection_name
+        super().__init__(f"Collection '{collection_name}' not found in Chroma DB.")
+
+class ChunkIDInvalidException(Exception):
+    """Raised when chunk with id specified is not found"""
+    def __init(self, chunk_id: str):
+        self.chunk_id = chunk_id
+        super().__init__(f"Chunk id '{chunk_id}' not found in collection.")
+
+# ====================
 # user exceptions
 # ====================
 
